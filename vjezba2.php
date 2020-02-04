323f9c8264282ba01486b6f9d97ed55627512eaa
lejlaidrizovic <58818277+lejlaidrizovic@users.noreply.github.com>
2020-02-04 19:50:25 +0100

    Add files via upload

---
+++ vjezba2.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="process2.php"  method="POST">
+    <label for="username"> Unesi username: </label>
+    <input type="text" name="username" required>
+    <br>
+    <label for="password">Unesi pasword: </label>
+    <input type="password" name="password" required >
+    <br>
+    <input type="submit" value
+    </form>
+
+
+</body>
+</html>
